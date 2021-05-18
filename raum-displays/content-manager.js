16e21152c16e445b84dfe30d287bc6e518ba479b
leon.stolze <leon.stolze@gmail.com>
2021-05-18 18:56:43 +0200

    Letztes Update, Encoding nur mit UTF-8

--- raum-displays/content-manager.js
+++ raum-displays/content-manager.js
@@ -25,7 +25,9 @@ var getRoom = function (url) {
 
 function dataParsed(results) {
 	lastCsvData = results.data;
-	//console.log(lastCsvData);
+
+	//lastCsvData=fillEmpty(lastCsvData);
+	console.log(lastCsvData);
 	var d = new Date();
 	var roomData=[];
 	for(i=0; i<lastCsvData.length; i++) {
@@ -33,13 +35,48 @@ function dataParsed(results) {
 		var m = lastCsvData[i].rs_end.split(":")[1];
 		//console.log(h+":"+m);
 		//console.log(d.getHours()+":"+d.getMinutes());
+		if((room==3||room==4)&&lastCsvData[i].rs_roomnum==5)
+			lastCsvData[i].rs_roomnum=room;
+
 		if(lastCsvData[i].rs_roomnum!=room||d.getHours()>h||(d.getHours()==h&&d.getMinutes()>m))
 			continue;
 		roomData.push(lastCsvData[i]);	
 	}
 	//console.log(roomData);
+	roomData=fillEmpty(roomData);
+	//console.log(roomData);
 	displayText(roomData, d);
 	
+}
+
+function minutes_with_leading_zeros(dt) {
+	return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+}
+
+function hours_with_leading_zeros(dt) { 
+	return (dt.getHours() < 10 ? '0' : '') + dt.getHours();
+}
+
+function setClock(date) {
+	document.getElementById("clock").textContent = hours_with_leading_zeros(date)+":"+minutes_with_leading_zeros(date);
+}
+
+function fillEmpty(data) {
+	//data[0].rs_begin,data[0].rs_end, data[0].rs_event, data[0].rs_group, data[0].rs_info;
+	
+	for(i=0; i<data.length; i++) {
+		if(data[i].rs_begin==null)
+			data[i].rs_begin="00:00";
+		if(data[i].rs_end==null)
+			data[i].rs_end="23:59";
+		if(data[i].rs_event==null)
+			data[i].rs_event="Unbelegt"
+		if(data[i].rs_group==null)
+			data[i].rs_group="Unbelegt"
+		if(data[i].rs_info==null)
+			data[i].rs_info="Raum in diesem Zeitraum nicht belegt"
+	}
+	return data;
 }
 
 function displayText(data, date) {
@@ -49,8 +86,10 @@ function displayText(data, date) {
 		updateAnzeige02(data[1].rs_begin,data[1].rs_end, data[1].rs_event, data[1].rs_group, data[1].rs_info);
 	if(data[2])
 		updateAnzeige03(data[2].rs_begin,data[2].rs_end, data[2].rs_event);
-	document.getElementById("clock").textContent = date.getHours()+":"+date.getMinutes();
+	setClock(date);
 }
+
+
 
 /*url oder Pfad zur .csv-Datei im Format:
  Raumbezeichnung,Uhrzeit Beginn,Uhrzeit Ende,Gruppe,Titel,Infotext
